206b167077b2d28072b94c60991d1ba69669cf46
Nathaniel Rongier <nath.rongier2002@gmail.com>
2021-03-02 11:01:55 +0100

    Arene is now showing the Arena, and now have a text box behind (to be a chat system in the future)!

--- src/vue/Arene.java
+++ src/vue/Arene.java
@@ -1,26 +1,64 @@
 package vue;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.net.URL;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
 
 public class Arene extends JFrame {
 
+	/**
+	 * Panel général
+	 */
 	private JPanel contentPane;
+	/**
+	 * Zone de saisie du t'chat
+	 */
+	private JTextField txtSaisie;
+	/**
+	 * Zone d'affichage du t'chat
+	 */
+	private JTextArea txtChat ;
 
 	/**
 	 * Create the frame.
 	 */
 	public Arene() {
+		this.getContentPane().setPreferredSize(new Dimension(800, 600 + 25 + 140));
+		this.pack();
+		this.setResizable(false);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(null);
 		setContentPane(contentPane);
+		
+		JLabel lblFond = new JLabel("");
+		lblFond.setIcon(new ImageIcon("C:\\Users\\nathr\\OneDrive\\Documents\\Cours\\Bloc 2\\Comp\u00E9tence 1\\S\u00E9quence 2\\S\u00E9ance 3\\Urban_Marginal_SQ2_S3_correction\\Urban_Marginal\\media\\fonds\\fondarene.jpg"));		
+		lblFond.setBounds(0, 0, 800, 600);
+		contentPane.add(lblFond);
+		
+		txtSaisie = new JTextField();
+		txtSaisie.setBounds(0, 600, 800, 25);
+		contentPane.add(txtSaisie);
+		txtSaisie.setColumns(10);
+		
+		JScrollPane jspChat = new JScrollPane();
+		jspChat.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		jspChat.setBounds(0, 625, 800, 140);
+		contentPane.add(jspChat);
+		
+		txtChat = new JTextArea();
+		jspChat.setViewportView(txtChat);
 	}
 
 }
